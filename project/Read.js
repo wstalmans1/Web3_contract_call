3c686d3bb2e9d5b6cc30b009b52e24c3a500c864
Wim Stalmans <wim.stalmans@gmail.com>
2024-04-20 13:01:26 +0200

    adjustmet read.js

--- project/Read.js
+++ project/Read.js
@@ -1,18 +1,15 @@
 require('dotenv').config({ path: '../.env' });
-const Web3 = require('web3');
+const { Web3 } = require('web3');
 
 async function main() {
+    
     // Setup the provider
     const web3 = new Web3(new Web3.providers.HttpProvider(`https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`));
     
-    // Contract ABI and Address
-    const contractABI = [{"inputs":[],"name":"count","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"decr","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"get","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"inc","outputs":[],"stateMutability":"nonpayable","type":"function"}];
-    const contractAddress = '0xF98569F595Df44242c34329c6F4d17221c71107b';
-    
     // Create contract instance
     const contract = new web3.eth.Contract(
-        contractABI, 
-        contractAddress);
+        [{"inputs":[],"name":"count","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"decr","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"get","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"inc","outputs":[],"stateMutability":"nonpayable","type":"function"}],
+        '0xF98569F595Df44242c34329c6F4d17221c71107b');
 
     // Interact with the contract
     const count = await contract.methods.get().call();
